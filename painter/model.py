431f23adb8d8bd0e2b19eae2cb45701309b8013b
B12_309_13\b12s309 <dzabalab258@gmail.com>
2024-08-06 11:09:25 -0500

    Cree las respectivas clases point y circle. Defini una función para hallar el area del circulo (pi * radio al cuadrado)

--- painter/model.py
+++ painter/model.py
@@ -1,1 +1,30 @@
 # TODO: Add code here
+import matplotlib
+
+
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x: float = x
+        self.y: float = y
+
+class circle:
+    def __init__(self, center: Point, radius: float):
+        self.center: Point = center
+        self.radius: float = radius
+
+area = 0
+pi = 3.1416
+def areacirculo (radio):
+    area= pi * (radio **2)
+    return area
+
+Rad = float (input("Por favor ingrese el radio"))
+Are= areacirculo(Rad)
+print ("El area del circulo es: ",  Are)
+
+def draw():
+    circle = matplotlib.circle((self.center.x, self.center.y), self.radius, color="r")
+    matplotlib.gca().add_patch(circle)
+    matplotlib.axis("scaled")
+    matplotlib.show()
+
