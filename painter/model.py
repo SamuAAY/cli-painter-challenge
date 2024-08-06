72302c8bb65fea3c1bbd7b1ecd34bbe24143634a
B12_309_13\b12s309 <dzabalab258@gmail.com>
2024-08-06 11:44:53 -0500

    Se define la clase triangulo con sus respectivos tres puntos. Continuando el codigo se define el area, se cre el metodo draw que agarre valores self y se define el str que devuelve la representacion en cadena

--- painter/model.py
+++ painter/model.py
@@ -1,5 +1,5 @@
 # TODO: Add code here
-import matplotlib
+import matplotlib.pyplot as plt
 
 
 class Point:
@@ -7,24 +7,59 @@ class Point:
         self.x: float = x
         self.y: float = y
 
-class circle:
+class Circle:
     def __init__(self, center: Point, radius: float):
         self.center: Point = center
         self.radius: float = radius
 
-area = 0
-pi = 3.1416
-def areacirculo (radio):
-    area= pi * (radio **2)
-    return area
+    area = 0
+    pi = 3.1416
 
-Rad = float (input("Por favor ingrese el radio"))
-Are= areacirculo(Rad)
-print ("El area del circulo es: ",  Are)
+    def area(self):
+        area = pi * (self.radius ** 2)
+        return area
 
-def draw():
-    circle = matplotlib.circle((self.center.x, self.center.y), self.radius, color="r")
-    matplotlib.gca().add_patch(circle)
-    matplotlib.axis("scaled")
-    matplotlib.show()
+    Rad = float(input("Por favor ingrese el radio"))
+    Are = area(Rad)
+    print("El area del circulo es: ", Are)
+
+    def draw(self) :
+        circle = plt.circle((self.center.x, self.center.y), self.radius, color="r")
+        plt.gca().add_patch(circle)
+        plt.axis("scaled")
+        plt.show()
+
+    def __str__(self):
+        Circle
+        return f"with center at {self.x}, {self.y} and {self.radius}"
+
+
+class Triangle:
+    def __init__(self, point_1:Point, point_2: Point, point_3: Point):
+        self.point_1: Point = point_1
+        self.point_2: Point = point_2
+        self.point_1: Point = point_3
+
+    def area(self):
+        area= (base * altura) / 2
+        return area
+
+    base= float(input("Ingrese la base del triangulo"))
+    altura= float(input("Ingrese la base"))
+
+    def draw(self):
+        x = [self.point_1.x, self.point_2.x, self.point_3.x, self.point_1.x]
+        y = [self.point_1.y, self.point_2.y, self.point_3.y, self.point_1.y]
+        plt.fill(x, y, color='b')
+        plt.axis("scaled")
+        plt.show()
+
+    def __str__(self):
+        Triangle
+        return f"with vertices at {self.x1, self.y1}, {self.x2, self.y2} and {self.x3, self.y3}"
+
+
+
+
+
 
