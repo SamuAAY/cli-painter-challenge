fee2baed5441d6a5ad96e17325f3956dc177c959
Samuel Alvarez <samus2978@gmail.com>
2024-08-08 11:01:52 -0500

    Added class rectangle

--- painter/model.py
+++ painter/model.py
@@ -1,4 +1,6 @@
 # TODO: Add code here
+import math
+import pickle
 import matplotlib.pyplot as plt
 
 
@@ -7,45 +9,35 @@ class Point:
         self.x: float = x
         self.y: float = y
 
+
 class Circle:
     def __init__(self, center: Point, radius: float):
         self.center: Point = center
         self.radius: float = radius
 
-    area = 0
-    pi = 3.1416
+    def area(self) -> float:
+        return math.pi * self.radius ** 2
 
-    def area(self):
-        area = pi * (self.radius ** 2)
-        return area
-
-    Rad = float(input("Por favor ingrese el radio"))
-    Are = area(Rad)
-    print("El area del circulo es: ", Are)
-
-    def draw(self) :
+    def draw(self):
         circle = plt.circle((self.center.x, self.center.y), self.radius, color="r")
         plt.gca().add_patch(circle)
         plt.axis("scaled")
         plt.show()
 
-    def __str__(self):
-        Circle
-        return f"with center at {self.x}, {self.y} and {self.radius}"
+    def __str__(self) -> str:
+        return f"Circle with center at ({self.center.x}, {self.center.y}) and radius {self.radius}"
 
 
 class Triangle:
-    def __init__(self, point_1:Point, point_2: Point, point_3: Point):
+    def __init__(self, point_1: Point, point_2: Point, point_3: Point):
         self.point_1: Point = point_1
         self.point_2: Point = point_2
-        self.point_1: Point = point_3
+        self.point_3: Point = point_3
 
-    def area(self):
-        area= (base * altura) / 2
-        return area
-
-    base= float(input("Ingrese la base del triangulo"))
-    altura= float(input("Ingrese la base"))
+    def area(self) -> float:
+        return 0.5 * abs((self.point_1.x * self.point_2.y - self.point_1.x * self.point_3.y) +
+                         (self.point_2.x * self.point_3.y - self.point_2.x * self.point_1.y) +
+                         (self.point_3.x * self.point_1.y - self.point_3.x * self.point_2.y))
 
     def draw(self):
         x = [self.point_1.x, self.point_2.x, self.point_3.x, self.point_1.x]
@@ -55,11 +47,23 @@ class Triangle:
         plt.show()
 
     def __str__(self):
-        Triangle
-        return f"with vertices at {self.x1, self.y1}, {self.x2, self.y2} and {self.x3, self.y3}"
+        return f"Triangle with vertices at {self.point_1.x, self.point_1.y}, {self.point_2.x, self.point_2.y}, and {self.point_3.x, self.point_3.y}"
 
 
+class Rectangle:
+    def __init__(self, point_1: Point, point_2: Point):
+        self.point_1: Point = point_1
+        self.point_2: Point = point_2
 
+    def area(self) -> float:
+        return abs((self.point_1.x - self.point_2.x) * (self.point_1.y - self.point_2.y))
 
+    def draw(self):
+        x = [self.point_1.x, self.point_2.x, self.point_2.x, self.point_1.x, self.point_1.x]
+        y = [self.point_1.y, self.point_1.y, self.point_2.y, self.point_2.y, self.point_1.y]
+        plt.fill(x, y, color='g')
+        plt.axis("scaled")
+        plt.show()
 
-
+    def __str__(self):
+        return f"Rectangle with vertices at {self.point_1.x, self.point_1.y} and {self.point_2.x, self.point_2.y}"
