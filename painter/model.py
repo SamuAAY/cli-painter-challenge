df80cc854cb459f2fcf319246beee83667513ef1
Samuel Alvarez <samus2978@gmail.com>
2024-08-08 11:21:25 -0500

    Added class Painter

--- painter/model.py
+++ painter/model.py
@@ -19,7 +19,7 @@ class Circle:
         return math.pi * self.radius ** 2
 
     def draw(self):
-        circle = plt.circle((self.center.x, self.center.y), self.radius, color="r")
+        circle = plt.Circle((self.center.x, self.center.y), self.radius, color="r")
         plt.gca().add_patch(circle)
         plt.axis("scaled")
         plt.show()
@@ -67,3 +67,33 @@ class Rectangle:
 
     def __str__(self):
         return f"Rectangle with vertices at {self.point_1.x, self.point_1.y} and {self.point_2.x, self.point_2.y}"
+
+class Painter:
+
+FILE = ".painter"
+
+def __init__(self) -> None:
+    self.shapes: list = []
+    self._load()
+
+def _load(self) -> None:
+    try:
+        with open(Painter.FILE, "rb") as f:
+            self.shapes = pickle.load(f)
+    except (EOFError, FileNotFoundError):
+        self.shapes = []
+
+def _save(self) -> None:
+    with open(Painter.FILE, "wb") as f:
+        pickle.dump(self.shapes, f)
+
+def add_shape(self, shape) -> None:
+    self.shapes.append(shape)
+    self._save()
+
+def total_area(self) -> float:
+    return sum(shape.area() for shape in self.shapes)
+
+def clear(self) -> None:
+    self.shapes = []
+    self._save()
